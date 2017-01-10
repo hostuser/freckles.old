f91d6af9437a3d3449a4a3ea49b51fa7c2d03078
Markus Binsteiner <makkus@gmail.com>
2017-01-11 10:39:42 +1300

    Documentation

---
+++ vagrant/debian_jessie64/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# bootstrap environment
+/freckles/bootstrap/bootstrap_deb.sh
+
+echo 'source "$HOME/.freckles/virtualenv/freckles/bin/activate"' >> "$HOME/.bashrc"
+ln -s /freckles/examples/example_only_zsh/dotfiles /home/vagrant/dotfiles
+
+# install freckles
+source "$HOME/.freckles/virtualenv/freckles/bin/activate"
+cd /freckles
+python setup.py develop
