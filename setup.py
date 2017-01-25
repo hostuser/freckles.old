f582409fc329ed8703b00c3ef88ac88eb3cd21ca
Markus Binsteiner <makkus@gmail.com>
2017-01-25 17:28:45 +1300

    Version upped

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='freckles',
-    version='0.1.3',
+    version='0.1.4',
     description="A dotfile manager",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
