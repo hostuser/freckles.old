7c740ba730f30d74764a47102645516e47f1852a
Markus Binsteiner <makkus@gmail.com>
2017-02-16 14:02:11 +1300

    git subrepo clone git@github.com:makkus/cookiecutter-freckles-play.git freckles/cookiecutter/external_templates/cookiecutter-freckles-play
    subrepo:
      subdir:   "freckles/cookiecutter/external_templates/cookiecutter-freckles-play"
      merged:   "3321490"
    upstream:
      origin:   "git@github.com:makkus/cookiecutter-freckles-play.git"
      branch:   "master"
      commit:   "3321490"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ freckles/cookiecutter/external_templates/cookiecutter-freckles-play/{{cookiecutter.execution_dir}}/freckles_run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi
+
+cd {{cookiecutter.freckles_playbook_dir}}
+
+ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_playbook}}
