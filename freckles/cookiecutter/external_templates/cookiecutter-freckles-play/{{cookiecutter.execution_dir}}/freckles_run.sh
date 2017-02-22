4a9bb3309414b692a81d56d3dbcd1b39f0915438
Markus Binsteiner <makkus@gmail.com>
2017-02-22 13:54:06 +1300

    Adding freckles path before system path in ansible run script.

--- freckles/cookiecutter/external_templates/cookiecutter-freckles-play/{{cookiecutter.execution_dir}}/freckles_run.sh
+++ freckles/cookiecutter/external_templates/cookiecutter-freckles-play/{{cookiecutter.execution_dir}}/freckles_run.sh
@@ -2,6 +2,8 @@
 
 if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi
 
+export PATH="$HOME/.freckles/bin:$PATH"
+
 cd {{cookiecutter.freckles_playbook_dir}}
 
 ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_playbook}}
