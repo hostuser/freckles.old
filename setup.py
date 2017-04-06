36e52439f0cb2cb6dc1764d4acf44f0640da9b6d
Markus Binsteiner <makkus@gmail.com>
2017-04-06 13:01:23 +0800

    Bump version: 0.1.34 → 0.1.35

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='freckles',
-    version='0.1.34',
+    version='0.1.35',
     description="A dotfile manager",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
