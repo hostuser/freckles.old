4516b8fa550a2e95d0efbc8d0feb3f6bb4747248
Markus Binsteiner <makkus@gmail.com>
2017-04-12 11:03:58 +0800

    Bump version: 0.1.40 → 0.1.41

--- freckles/__init__.py
+++ freckles/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Markus Binsteiner"""
 __email__ = 'makkus@posteo.de'
-__version__ = '0.1.40'
+__version__ = '0.1.41'
 
 from freckles import Freck, Freckles, FrecklesRunCallback
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='freckles',
-    version='0.1.40',
+    version='0.1.41',
     description="A dotfile manager",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
