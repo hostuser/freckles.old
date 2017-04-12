dbf2b6dc4651bac9f07771483799da3214299bc1
Markus Binsteiner <makkus@gmail.com>
2017-04-12 10:45:38 +0800

    Fix for systems where default python (2) is not locatated in /usr/bin/python

--- freckles/runners/ansible_runner.py
+++ freckles/runners/ansible_runner.py
@@ -76,7 +76,7 @@ def create_inventory_dir(hosts, inventory_dir, group_name=FRECKLES_DEFAULT_GROUP
 
 {}
 
-""".format(group_name, "\n".join(hosts.keys())))
+""".format(group_name, "\n".join("{}     {}".format(hosts.keys(), ansible_python_interpreter='/usr/bin/env python')))
 
 def create_playbook_dict(playbook_items, host_group=FRECKLES_DEFAULT_GROUP_NAME):
     """Assembles the dictionary to create the playbook from."""
